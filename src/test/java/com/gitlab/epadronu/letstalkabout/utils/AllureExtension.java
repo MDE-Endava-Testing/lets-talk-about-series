4ae6d0de41a76289a60bc77d30f949d35079315c
Edinson E. Padrón Urdaneta <edinson.padron@posteo.net>
2021-04-09 21:04:10 -0500

    Create and setup the project

---
+++ src/test/java/com/gitlab/epadronu/letstalkabout/utils/AllureExtension.java
@@ -0,0 +1,14 @@
+package com.gitlab.epadronu.letstalkaboutassertj.utils;
+
+import io.qameta.allure.Allure;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestWatcher;
+
+public class AllureExtension implements TestWatcher {
+
+  @Override
+  public void testFailed(final ExtensionContext context, final Throwable cause) {
+    Allure.addAttachment("Exception", ExceptionUtils.getStackTrace(cause));
+  }
+}
---
+++ src/test/java/com/gitlab/epadronu/letstalkabout/utils/PascalCaseDisplayNameGenerator.java
@@ -0,0 +1,31 @@
+package com.gitlab.epadronu.letstalkaboutassertj.utils;
+
+import java.lang.reflect.Method;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+public class PascalCaseDisplayNameGenerator implements DisplayNameGenerator {
+
+  @Override
+  public String generateDisplayNameForClass(final Class<?> aClass) {
+    return pascalCaseToSpaceSeparatedPascalCase(aClass.getSimpleName());
+  }
+
+  private static String pascalCaseToSpaceSeparatedPascalCase(final String name) {
+    return StringUtils.capitalize(
+     String
+      .join(" ", StringUtils.splitByCharacterTypeCamelCase(name))
+      .toLowerCase());
+  }
+
+  @Override
+  public String generateDisplayNameForNestedClass(final Class<?> aClass) {
+    return pascalCaseToSpaceSeparatedPascalCase(aClass.getSimpleName());
+  }
+
+  @Override
+  public String generateDisplayNameForMethod(final Class<?> aClass, final Method method) {
+    return pascalCaseToSpaceSeparatedPascalCase(method.getName());
+  }
+}
